9f4d144b48b51165575c654eaba29a2017a89ef7
8937 <yanjun@adpanshi.com>
2018-12-13 17:33:22 +0800

    master

---
+++ app/src/main/java/com/nujnay/slidcard/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.nujnay.slidcard;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+    }
+}
