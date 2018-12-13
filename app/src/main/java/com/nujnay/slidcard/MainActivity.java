91ac497961a7c14311a5759cf4187a1c2ab84e95
8937 <yanjun@adpanshi.com>
2018-12-13 17:20:27 +0800

    master

--- app/src/main/java/com/nujnay/slidcard/MainActivity.java
+++ app/src/main/java/com/nujnay/slidcard/MainActivity.java
@@ -12,26 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.behavior).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-                    view.setX(motionEvent.getRawX() - view.getWidth() / 2);
-                    view.setY(motionEvent.getRawY() - view.getHeight() / 2);
-                }
-                return true;
-            }
-        });
-
-//        findViewById(R.id.right).setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-//                if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-//                    view.setX(motionEvent.getRawX() - view.getWidth() / 2);
-//                    view.setY(motionEvent.getRawY() - view.getHeight() / 2);
-//                }
-//                return true;
-//            }
-//        });
+        TextView
     }
 }
