28bdb58c57f0a2b1748f261c407d37f423e2fa64
8937 <yanjun@adpanshi.com>
2018-12-13 17:49:12 +0800

    master

--- app/src/main/java/com/nujnay/slidcard/FollowBehavior.java
+++ app/src/main/java/com/nujnay/slidcard/FollowBehavior.java
@@ -3,15 +3,13 @@ package com.nujnay.slidcard;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class FollowBehavior extends CoordinatorLayout.Behavior<TestView> {
 
-    /**
-     * 构造方法
-     */
     public FollowBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -23,6 +21,7 @@ public class FollowBehavior extends CoordinatorLayout.Behavior<TestView> {
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, TestView child, View dependency) {
+        Log.d("onDependcdd", "1111111");
         child.setX(dependency.getX());
         child.setY(dependency.getY() + 100);
         return true;
