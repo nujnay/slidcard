8929108187c5bcedbbd9e207563c4549b85fb421
8937 <yanjun@adpanshi.com>
2018-12-14 11:58:56 +0800

    111

--- app/src/main/java/com/nujnay/slidcard/FollowBehavior.java
+++ app/src/main/java/com/nujnay/slidcard/FollowBehavior.java
@@ -9,6 +9,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class FollowBehavior extends CoordinatorLayout.Behavior<View> {
+    private int leftTop_Y_Max = ScreenTools.dp2px(Myapplication.myapplication, 32);
+    private int leftTop_X_Max = ScreenTools.dp2px(Myapplication.myapplication, 23);
+    private int parentWith = ScreenTools.getScreenWidth() - ScreenTools.dp2px(Myapplication.myapplication, 13) * 2;
+    private int yLengthMix = ScreenTools.dp2px(Myapplication.myapplication, 134);
+    private int yChangeTotalLength = ScreenTools.dp2px(Myapplication.myapplication, 20);
+    private int yLengthMax = ScreenTools.dp2px(Myapplication.myapplication, 154);
 
     public FollowBehavior() {
 
@@ -29,6 +35,14 @@ public class FollowBehavior extends CoordinatorLayout.Behavior<View> {
         float dependencyY = dependency.getY();
         float dependencyWidth = dependency.getWidth();
         float dependencyHeight = dependency.getHeight();
+
+        float childLeftTopY = leftTop_Y_Max - dependencyY;
+        float childLeftTopX = leftTop_X_Max - dependencyX;
+
+        float childRightBottomY = yLengthMix + ((yLengthMix -dependencyHeight ))+(yChangeTotalLength-(dependencyY-dependencyHeight));
+        float childRightBottomX = parentWith - (leftTop_X_Max - dependencyX);
+
+        child.layout((int) childLeftTopX, (int) childLeftTopY, (int) childRightBottomX, (int) childRightBottomY);
 
         Log.d("dependencyXdd", dependencyX + "||"
                 + dependencyY + "||"
