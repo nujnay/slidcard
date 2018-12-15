43c280f2ae1f94dcada8023a03f52b2b3c745f21
8937 <yanjun@adpanshi.com>
2018-12-15 14:59:03 +0800

    111

--- app/src/main/java/com/nujnay/slidcard/MainActivity2.java
+++ app/src/main/java/com/nujnay/slidcard/MainActivity2.java
@@ -5,14 +5,11 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
-    CoordinatorLayout cl_main;
-    TestView observer;
-    TestView behavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
     }
 }
